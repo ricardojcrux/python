ae9865d89b97c0ea5771ec507831c9615af66d68
Ricardo Zapata Cruz <ricardozapatacruz@gmail.com>
2023-02-11 16:53:33 -0500

    cambios archivo tools en la carpeta app

--- app/tools.py
+++ app/tools.py
@@ -1,13 +1,13 @@
 def get_population(data):
 	dict = {
-		'2022':int(data['2022 Population']),
-		'2020':int(data['2020 Population']),
-		'2015':int(data['2015 Population']),
-		'2010':int(data['2010 Population']),
-		'2000':int(data['2000 Population']),
-		'1990':int(data['1990 Population']),
+		'1970':int(data['1970 Population']),
 		'1980':int(data['1980 Population']),
-		'1970':int(data['1970 Population'])
+		'1990':int(data['1990 Population']),
+		'2000':int(data['2000 Population']),
+		'2010':int(data['2010 Population']),
+		'2015':int(data['2015 Population']),
+		'2020':int(data['2020 Population']),
+		'2022':int(data['2022 Population'])
 		}
 	return dict.keys(), dict.values()
 
