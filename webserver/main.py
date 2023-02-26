2ce41fdd89bb656c8daa4ef65e0cc6419a013b27
Ricardo Zapata Cruz <ricardozapatacruz@gmail.com>
2023-02-26 18:42:09 -0500

    cambios en main webserver

--- webserver/main.py
+++ webserver/main.py
@@ -1,4 +1,19 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return [1,2,3]
+
+@app.get('/contact',response_class=HTMLResponse)
+def get_list():
+    return """
+    <h1>Welcome to new contact</h1>
+    <p>You always are welcome here</p>
+    """
 
 def run():
     store.get_categories()
